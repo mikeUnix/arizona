8c071fc9e956c5d6f12f8d5bc6ea3f58fdcba733
Mike Unix <masteranisimov@gmail.com>
2020-04-03 21:26:27 +0300

    lastversion 21:26

--- myenv/app/projectArizona/projectArizona/urls.py
+++ myenv/app/projectArizona/projectArizona/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
-from .views import exampleFunction
+#from .views import exampleFunction
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Arizona/', exampleFunction)
+    path('Arizona/', include('Arizona.urls'))
 ]
