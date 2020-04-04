9a91826ab05f318477fd36aa8d554be1b1937f87
Mike Unix <masteranisimov@gmail.com>
2020-04-04 16:37:22 +0300

    ostanovils'ya na uroke3 15:21

--- myenv/app/projectArizona/Arizona/views.py
+++ myenv/app/projectArizona/Arizona/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
 def post_list(request):
-	return HttpResponse('<h1>zdorova ot korovu !!! <h2>')
+	a=['KotVasya','Koshka','Murka','Shalashovka']
+	return render(request,'Arizona/index.html',context={'names':a})
