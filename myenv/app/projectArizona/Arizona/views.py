4fc32ae2a4e3cfe440ca67439c48512c577a3686
Mike Unix <masteranisimov@gmail.com>
2020-04-08 15:34:58 +0300

    nerabotaet kopka reed

--- myenv/app/projectArizona/Arizona/views.py
+++ myenv/app/projectArizona/Arizona/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import View
 from .models import Post, Tag
 
 # Create your views here.
@@ -8,10 +9,28 @@ def posts_list(request):
 	return render(request,'Arizona/index.html',context={'posts':posts})
 
 
-def post_detail(request,slug):
-	post = Post.objects.get(slug__iexact=slug)
-	return render(request, 'Arizona/post_detail.html',context={'post':post})
-
+#def post_detail(request,slug):
+#	post = Post.objects.get(slug__iexact=slug)
+#	return render(request, 'Arizona/post_detail.html',context={'post':post})
+#######
 def tags_list(request):
 	tags = Tag.objects.all()
 	return render(request,'Arizona/tags_list.html',context={'tags':tags})
+
+#def tag_detail(request,slug):
+#	tag = Tag.objects.get(slug__iexact=slug)
+#	return render(request, 'Arizona/tag_detail.html', context={'tag':tag})
+
+class PostDetail(View):
+	def get(self,reqest,slug):
+		post = Post.objects.get(slug__iexact=slug)
+		return render(request, 'Arizona/post_detail.html',context={'post':post})
+
+class TagDetail(View):
+	def get(self,request,slug):
+		tag = Tag.objects.get(slug__iexact=slug)
+		return render(request, 'Arizona/tag_detail.html', context={'tag':tag})
+
+
+		
+
