17556893598520cae32815ee4217b2d8f882d56a
Mike Unix <masteranisimov@gmail.com>
2020-05-01 14:27:16 +0300

    finality

--- myenv/app/projectArizona/Arizona/views.py
+++ myenv/app/projectArizona/Arizona/views.py
@@ -8,14 +8,22 @@ from .utils import*
 from .forms import TagForm , PostForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
-
+from django.db.models import Q
 
 
 # Create your views here.
 
 def posts_list(request):
-	posts = Post.objects.all()
+	search_query = request.GET.get('search','')
+	if search_query:
+		posts = Post.objects.filter(Q(title__icontains=search_query) or Q(body__icontains=search_query))
+	else:
+		posts = Post.objects.all()
+	
 	paginator = Paginator(posts, 10)
+
+	
+
 	page_number = request.GET.get('page',1)
 	page = paginator.get_page(page_number)
 	is_paginated = page.has_other_pages()
